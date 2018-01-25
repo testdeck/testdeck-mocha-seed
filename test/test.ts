6d19473c27d2d519d0480fd562f92863da4f4a15
PanayotCankov <panayot.cankov@telerik.com>
2018-01-25 20:01:11 +0200

    Add root level hooks

--- test/test.ts
+++ test/test.ts
@@ -4,21 +4,36 @@
 import { Unit } from "../src/index";
 import { assert } from "chai";
 
+declare var console;
+
+before("start server", () => {
+    // Run express?
+    console.log("start server");
+});
+after("kill server", () => {
+    // Kill the server.
+    console.log("kill server");
+});
+
 describe("vanilla bdd", () => {
-    it("test", () => {
+    it("test", async () => {
+        await console.log("  vanilla bdd test");
     });
 });
 
 suite("vanilla tdd", () => {
-    test("test", () => {
+    test("test", async () => {
+        await console.log("  vanilla tdd test");
     });
 });
 
 @suite class UnitTest extends Unit {
     @test "big is true with big number"() {
+        console.log("  UnitTest big is true with big number");
         assert(this.big(200));
     }
     @test "big is false with small number"() {
+        console.log("  UnitTest big is false with small number");
         assert(!this.big(50));
     }
 }
@@ -26,6 +41,7 @@ suite("vanilla tdd", () => {
 suite("nested class suite", () => {
     @suite class NestedTest {
         @test "a test"() {
+            console.log("  nested class suite NestedTest a test");
         }
     }
 });
